079375a886c7220b3dbb91827dc8a3b8377e1981
Jolanta Gryko <jgryko@gmail.com>
2024-12-17 14:17:43 +0100

    16.12

--- tizbd/02_normalizacja/03_2NF.sql
+++ tizbd/02_normalizacja/03_2NF.sql
@@ -6,36 +6,51 @@
 -- język
 -- adres
 
-CREATE TABLE Pracownicy2(
+CREATE TABLE Pracownicy3(
     idPracownika int,
     nazwisko varchar(255),
     imie varchar(255),
-    jezyk varchar(255),
     ulica varchar(255),
     numer_domu varchar(255),
     miejscowosc varchar(255)
 );
+CREATE TABLE Jezyki(
+    idPracownika int,
+    jezyk varchar(255)
+);
+ALTER TABLE Pracownicy3
+ADD PRIMARY KEY(idPracownika);
 
-
+ALTER TABLE Jezyki
+ADD PRIMARY KEY(idPracownika,Jezyk);
 
 -- 3. wpisz dane:
-INSERT INTO Pracownicy2
-(idPraocwnika,imie, nazwisko, jezyk, ulica, numer_domu, miejscowosc)
+INSERT INTO Pracownicy3
+(idPracownika, imie, nazwisko, ulica, numer_domu, miejscowosc)
 VALUES
-(1, 'Jan', 'Kowalski', 'PHP', 'os. Wl. Lokietka', '3/4', 'Naklo nad Notecia'),
- (2, 'Antoni', 'Malinowski', 'HTML','','1', 'Chrzastowo'),
- (2, 'Antoni', 'Malinowski', 'CSS','','1', 'Chrzastowo'),
- (2, 'Antoni', 'Malinowski', 'PHP','','1', 'Chrzastowo'),
- (3, 'Jan', 'Malinowski','Java', 'ul. Ogrodowa', '2', 'Nowa Wies Wielka'),
- (3, 'Jan', 'Malinowski', 'HTML',  'ul. Ogrodowa', '2', 'Nowa Wies Wielka'),
-(4, 'Andrzej', 'Ziemianski', 'JavaScript',  'ul. Sowia', '5', 'Nowa Wies'),
-(4, 'Andrzej', 'Ziemianski',  'CSS', 'ul. Sowia', '5', 'Nowa Wies'),
-(4, 'Andrzej', 'Ziemianski',  'HTML', 'ul. Sowia', '5', 'Nowa Wies');
+(1, 'Jan', 'Kowalski', 'os. Wl. Lokietka', '3/4', 'Naklo nad Notecia'),
+ (2, 'Antoni', 'Malinowski','','1', 'Chrzastowo'),
+ (3, 'Jan', 'Malinowski', 'ul. Ogrodowa', '2', 'Nowa Wies Wielka'),
+(4, 'Andrzej', 'Ziemianski',  'ul. Sowia', '5', 'Nowa Wies');
 
+INSERT INTO Jezyki
+(idPracownika,jezyk)
+VALUES
+(1,'PHP'),
+ (2,'HTML'),
+ (2,'CSS'),
+ (2,'PHP'),
+ (3,'Java'),
+ (3,'HTML'),
+(4,'JavaScript'),
+(4,'CSS'),
+(4,'HTML');
 -- 4. Wykonaj zapytania
 
 -- A. Wypisz pracowników, którzy znają język PHP
-
+SELECT imie , nazwisko
+FROM Pracownicy3,Jezyki
+WHERE jezyk=PHP
 -- B. Wypisz pracowników, którzy znają język Java
 
 -- C. Wypisz pracowników mieszkających w Nowej Wsi
