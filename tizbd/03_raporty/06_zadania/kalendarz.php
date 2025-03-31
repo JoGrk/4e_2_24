e948b22e8370c9f2bfbec651fdc643f77e26288a
Jolanta Gryko <jgryko@gmail.com>
2025-03-31 14:09:13 +0200

    31.03

---
+++ tizbd/03_raporty/06_zadania/kalendarz.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mój kalendarz</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+    <header>
+        <section class="one">
+            <img src="logo1.png" alt="Mój kalendarz">
+        </section>
+
+
+        <section class="two">
+            <h1>kalendarz</h1>
+            <!-- skrypt 1 -->
+        </section>
+    </header>
+
+    <main>
+        <!-- skrypt 2 -->
+    </main>
+
+    <footer>
+        <p>Strone wykonał ###</p>
+    </footer>
+</body>
+</html>
