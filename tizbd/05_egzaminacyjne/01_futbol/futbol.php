6967102f2be810dfdf087df3e2f6a1a908edfdb1
Jolanta Gryko <jgryko@gmail.com>
2025-06-17 11:24:39 +0200

    17.16

--- tizbd/05_egzaminacyjne/01_futbol/futbol.php
+++ tizbd/05_egzaminacyjne/01_futbol/futbol.php
@@ -1,11 +1,25 @@
 <?php
 $link = new mysqli ('localhost','root','','4e_2_futbol');
 
+$position_f = $_POST['position_f'] ?? NULL;
+if($position_f){
+    $sql="SELECT imie,nazwisko,pozycja_id
+            FROM zawodnik
+            WHERE pozycja_id = $position_f;";
+    $result = $link -> query($sql);
+    $players = $result -> fetch_all(1);
+}
+
+
+
 $sql="SELECT zespol1,zespol2,wynik,data_rozgrywki
-FROM rozgrywka
-WHERE zespol1 = 'EVG';";
+    FROM rozgrywka
+    WHERE zespol1 = 'EVG';";
 $result = $link -> query($sql);
 $games = $result -> fetch_all(1);
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -24,9 +38,21 @@ $games = $result -> fetch_all(1);
 
     <section class="games">
         <!-- skrypt1 -->
-         <div class="game">
-            
-         </div>
+         <!-- <div class="game">
+            <h3>zespół1 - zespół2</h3>
+            <h4>wynik</h4>
+            <p>w dniu: <data rozgrywki></p>
+         </div> -->
+         <?php
+            foreach($games as $game){
+                echo"<div class='game'>
+                        <h3>{$game['zespol1']} - {$game['zespol2']}</h3>
+                        <h4>{$game['wynik']}</h4>
+                        <p>w dniu: {$game['data_rozgrywki']}</p>
+                     </div>";
+            }
+
+         ?>
     </section>
 
     <main>
@@ -37,11 +63,19 @@ $games = $result -> fetch_all(1);
         <section class="left">
             <p>Podaj pozycję zawodników (1-bramkarze, 2-obrońcy, 3-pomocnicy, 4-napastnicy):</p>
             <form action="" method="post">
-                <input type="number" name="position" id="position">
+                <input type="number" name="position_f" id="position">
                 <button>Wyślij</button>
             </form>
             <ul>
                 <!-- skrypt2 -->
+                <!-- <li><p>imie nazwisko</p></li>  -->
+                 <?php
+                    if($position_f){
+                        foreach( $players as $player){
+                            echo"<li><p>{$player['imie']} {$player['nazwisko']}</p></li>";
+                        }
+                    }
+                 ?>
             </ul>
         </section>
 
